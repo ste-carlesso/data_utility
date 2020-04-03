04acfda3ec3a2a8b8b87779e0b004ea58c980c45
Stefano Carlesso <s.carlesso@fondazioneomd.it>
2020-04-03 18:28:50 +0200

    Some progress.     - can read all CSV files     - can count files     - can find min and max dates     - can create excel file

--- main.py
+++ main.py
@@ -2,35 +2,94 @@
 read station index from data_folder/stazioni.csv into a stations_df
 
 read files from "data_folder/" + codice_stazione + ".csv" into 
-
-
 """
 import glob, os
 import numpy as np
 import pandas as pd
-
+debug = False
+# shorter experiments
+parse_one_file = False
+#input format
 input_csv_dialect =  {
     "field separator" : ";", 
     "decimal separator" : ".", 
     "quoted fields" : "no", 
     "newline" : ""
 }
-
 # stazioni.csv: UTF-8 Unicode text
-stations_metadata_file = "./dati/stazioni.csv"
 # single station data files are ASCII text
-metadata = pd.read_csv(stations_metadata_file, sep=";")
 
-lombardia_files = glob.glob("dati/lmb[0-9][0-9][0-9].csv")
-lombardia_df = pd.DataFrame()
+
+
+metadata_file = "./dati/stazioni.csv"
+
+metadata_df = pd.read_csv(metadata_file, sep=";")
+if parse_one_file:
+    lombardia_files = glob.glob("dati/lmb002.csv")
+else:
+    lombardia_files = glob.glob("dati/lmb[0-9][0-9][0-9].csv")
+lombardia_list = []
+
+dateparse = lambda x: pd.datetime.strptime(x, "%Y-%m-%d")
+first_day = pd.Timestamp("2020-01-01", freq="D")
+last_day = pd.Timestamp("1990-01-01", freq="D")
 
 for station_file in lombardia_files:
+    station_df = pd.read_csv(
+        filepath_or_buffer = station_file,
+        usecols = [1,2,3,4],
+        header = 0, 
+        sep=";",
+        decimal = ".",
+        parse_dates = ["datetime"],
+        date_parser = dateparse,
+        )
+    if debug == True:
+        print("file-> ",  station_file)
+        print("shape-> ",  station_df.shape)
+        if ("datetime" not in station_df.columns):
+            print("warning: no datetime field in ", station_file)
+        print("col data type\n", station_df.dtypes)
+    # rename columns
+    station_min_day = station_df["datetime"].min()
+    if station_min_day < first_day:
+        first_day = station_min_day
+    else:
+        pass
+    station_max_day = station_df["datetime"].max()
+    if station_max_day > last_day:
+        last_day = station_max_day
+    else:
+        pass
+    lombardia_list.append(station_df)
+print("first_day-> ",first_day)   
+print("last_day-> ",last_day)
+print("Lombardia; number of dataframes created -> ",len(lombardia_list))
+dates = pd.date_range(
+    start = first_day,
+    end = last_day,
+    freq = "D",
+)
+# np.full(shape=(len(dates),5), fill_value= -999,)
+lombardia_df = pd.DataFrame
+    # must put datetime label on index columns
+    data = None,
+    index = dates,
+)
+
+
+for station_df in lombardia_list:
+    lombardia_df = pd.merge(lombardia_df, station_df, on="datetime")
     
-    station_df = pd.read_csv(station_file, sep=";")
-    #print(station_df.shape)
-    station_columns = station_df.columns
-    if not (station_columns[1] == "datetime"):
-        print(station_file)
-        print("no datetime")
-    #lombardia_df = pd.merge(lombardia_df, station_df, on="datetime")
+"""
+output format
+An excel spreadsheet xlsx, with three tabs
+tabs = {"t_min":"minima", "t_med":"media", "t_max":"massima"}
+"""
+output_file = "temperature.xlsx"
+with pd.ExcelWriter(output_file) as writer:
+    lombardia_df.to_excel(
+        excel_writer = writer, 
+        sheet_name = "minima",
+    )
     
