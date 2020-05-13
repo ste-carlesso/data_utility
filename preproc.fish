9980daf6267af68c2121e14701b5943586ca37f6
Stefano Carlesso <s.carlesso@fondazioneomd.it>
2020-05-13 14:39:04 +0200

    Change preproc.fish. Output directory now is "input" (quite confusing, but new.py expects this.

--- preproc.fish
+++ preproc.fish
@@ -1,11 +1,12 @@
 # to be run in data_utility directory
 set list2 (ls --width=0 fomd2)
 echo $list2
-mkdir fomd3
-mv fomd1/* fomd3/ 
+mkdir input
+# the destination folder is called input because the new.py script expects this
+mv fomd1/* input/ 
 
 for f in $list2
     # output file starting from the second line
-    tail --lines=+2 fomd2/$f >> fomd3/$f
+    tail --lines=+2 fomd2/$f >> input/$f
 end
 
